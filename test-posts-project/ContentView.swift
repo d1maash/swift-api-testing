00f818c950c831d23dc661bcd6e80b86b0e7f10b
Dimash <dimashaijarikov@gmail.com>
2024-08-16 10:01:33 +0500

    update

--- test-posts-project/ContentView.swift
+++ test-posts-project/ContentView.swift
@@ -8,16 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State var posts: [Post] = []
+    
+    
+     var body: some View {
+        NavigationView {
+            List(posts) { post in
+                VStack {
+                    Text(post.title).fontWeight(.bold)
+                    Text(post.body)
+                }
+            }.onAppear() {
+                Api().getPost {
+                    (posts) in
+                    self.posts = posts
+                }
+            }.navigationTitle("Posts")
         }
-        .padding()
     }
 }
+
+struct Post: Codable, Identifiable {
+    let id = UUID()
+    var title: String
+    var body: String
+}
+    class Api {
+        func getPost (completion: @escaping ([Post]) -> ()) {
+            guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
+            else { return }
+            
+            URLSession.shared.dataTask(with: url) { (data, _, _) in
+                
+                let posts = try! JSONDecoder().decode([Post].self, from: data!)
+                DispatchQueue.main.async {
+                    completion(posts)
+                }
+                
+            }
+            .resume()
+        }
+    
+}
+
 
 #Preview {
     ContentView()
