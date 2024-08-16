2b6309d6c2f850c660dbfbd9c9ac49820a74023c
Dimash <dimashaijarikov@gmail.com>
2024-08-16 09:41:37 +0500

    Initial Commit

---
+++ test-posts-project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test-posts-project
+//
+//  Created by Динмуханбет Айжарыков on 16.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test-posts-project/test_posts_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_posts_projectApp.swift
+//  test-posts-project
+//
+//  Created by Динмуханбет Айжарыков on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct test_posts_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
